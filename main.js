60d7d78cccf779b431621014472e07abb408a7a6
Oliva <0731163@students.dadeschools.net>
2019-02-15 14:13:33 -0500

    car game finished logic

--- main.js
+++ main.js
@@ -3,33 +3,67 @@ let car ={
     make:'Lamborghini',
     model:'Countach',
     color:'white',
-    Millage:0,
+    millage:0,
     isWorking:true,
     driveToWork(){
-        alert(`Old Millage: ${this.Millage}`);
-        this.Millage += 10;
-        alert(`New Millage: ${this.Millage}`);
+        alert(`Old Millage: ${this.millage}`);
+        this.millage += 10;
+        alert(`New Millage: ${this.millage}`);
     },
-
-}
+    driveAroundUSA(){
+        alert(`Old Millage: ${this.millage}`);
+        this.millage += 7000;
+        alert(`New Millage: ${this.millage}`);
+        alert(`your car needs a tuneup!`);
+        this.isWorking=false;
+    },
+    getTuneUp(){
+        alert(`your car is fixed now!`);
+        this.isWorking=true;
+    },
+    honk(){
+        alert(`honk, honk`);
+    }
+};
 
 //FUNCTION
 //log all of our car's current stats to the console
-/*
-function reWriteStats(){
-
+reWriteStats = ()=>{
+    console.log(`make: ${car.make}`);
+    console.log(`model: ${car.model}`);
+    console.log(`color: ${car.color}`);
+    console.log(`millage: ${car.millage}`);
+    console.log(`is working: ${car.isWorking}`);
+    console.log(`--------------------------------------------`)
 }
-*/
+
 
 //MAIN PROCESS
 //Captures Keyboard input. Depending on the letter pressed it will "call" (execute) diferent functions.
 
-document.onkeyup = (event)=>{
+document.onkeyup = (event) => {
     //Captures the key pressed and convertes in to lower case and saves it to a variable
-    let letter = String.fromCharCode(event.KeyCode).toLowerCase();
+    let letter = String.fromCharCode(event.keyCode).toLowerCase();
 
-    if(letter==='d'){
-        car.driveToWork();
-        reWriteStats;
+    if (letter ==="t") {
+        car.getTuneUp();
+        reWriteStats();
     }
+    if(car.isWorking===true){
+    if (letter ==="d") {
+        car.driveToWork();
+        reWriteStats();
+    }
+    if (letter ==="r") {
+        car.driveAroundUSA();
+        reWriteStats();
+    }
+    if (letter ==="h") {
+        car.honk();
+        reWriteStats();
+    }
+
+}else{
+    console.log(`your car is not working! get a tune up.`)
+}
 }
